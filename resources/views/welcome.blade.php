40ed6f6db83692b482b5eb3f3473b77cb701e9f7
nellytadi <nellytadi@gmail.com>
2020-09-10 00:06:56 +0100

    Made chages to the Website

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -27,7 +27,7 @@
         <div id="particles-1" class="full-image" data-mask="50"></div>
 
         <!-- Media -->
-        <div class="parallax-x-bg" style="background-image:url(/assets/images/bg-parallax.png)"
+        <div class="parallax-x-bg" style="background-image:url('{{asset('assets/images/bg-parallax-1.png')}}')"
              data-swiper-parallax="-50%"></div>
 
         <div class="swiper-wrapper">
