873287f06124479c2249a46d894daca105e360e6
nellytadi <nellytadi@gmail.com>
2020-09-14 11:56:40 +0100

    Send email contact ud

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -8,74 +8,78 @@ class ContactController extends Controller
 {
     public function sendMail(Request $request)
     {
-        mail('nellytadi@gmail.com','Testing Website Email','Hello Nelly, Testing email');
-//        if ($request->has('email')) {
-//
-//            $name = $request->input('name');
-//            $email = $request->input('email');
-//            $phone = $request->input('phone');
-//            $subject = "Contact email from your Website";
-//            $message = $request->input('message');
-//
-//            // Form validation
-//            if (!empty($name) && !empty($email) && !empty($phone) && !empty($message)) {
-//
-//                // reCAPTCHA validation
-//                if ($request->has('g-recaptcha-response') && !empty($request->input('g-recaptcha-response') )) {
-//
-//                    // Google secret API
-//                    $secretAPIkey = '6LfoZcsZAAAAAAVQM3GgwYR30uIT6QFSz-CiJMSw';
-//
-//                    // reCAPTCHA response verification
-//                    $verifyResponse = file_get_contents('https://www.google.com/recaptcha/api/siteverify?secret=' . $secretAPIkey . '&response=' . $_POST['g-recaptcha-response']);
-//
-//                    // Decode JSON data
-//                    $response = json_decode($verifyResponse);
-//                    if ($response->success) {
-//
-//                        $toMail = "nellytadi@gmail.com";
+
+        if ($request->has('email')) {
+
+            $name = $request->input('name');
+            $email = $request->input('email');
+            $phone = $request->input('phone');
+            $subject = "Contact email from your Website";
+            $message = $request->input('message');
+
+            // Form validation
+            if (!empty($name) && !empty($email) && !empty($phone) && !empty($message)) {
+
+                // reCAPTCHA validation
+                if ($request->has('g-recaptcha-response') && !empty($request->input('g-recaptcha-response') )) {
+
+                    // Google secret API
+                    $secretAPIkey = '6LfoZcsZAAAAAAVQM3GgwYR30uIT6QFSz-CiJMSw';
+
+                    // reCAPTCHA response verification
+                    $verifyResponse = file_get_contents('https://www.google.com/recaptcha/api/siteverify?secret=' . $secretAPIkey . '&response=' . $_POST['g-recaptcha-response']);
+
+                    // Decode JSON data
+                    $response = json_decode($verifyResponse);
+                    if ($response->success) {
+
+                        $toMail = "info@baselineeducationalservices.com";
 //                        $header = "From: " . $name . "<" . $email . ">\r\n";
-//                        $header .= "MIME-Version: 1.0\r\n";
-//                        $header .= "Content-type: text/html\r\n";
-//
-//                        $body  = '<table style="padding: 35px; background-color: #f5f5f5; font-family: Roboto, sans-serif; font-size: 1rem; text-align: left; border-radius: 4px">';
-//                        $body .= '<tr><th style="font-size: 1.5rem; font-weight: 600; color: #1E50BC">'.$message.'</th></tr>';
-//                        $body .= '<tr><th style="font-size: 1.5rem; font-weight: 600; color: #1E50BC">'.$phone.'</th></tr>';
-//                        $body .= '</table>';
-//                        if(mail($toMail, $subject, $body, $header)){
-//                            $response = array(
-//                                "status" => "alert-success",
-//                                "message" => "Your mail has been sent. We will get back to you shortly."
-//                            );
-//                        }
-//                        else {
-//                            $response = array(
-//                                "status" => "alert-danger",
-//                                "message" => "Oops, something went wrong. Please try again."
-//                            );
-//                        }
-//
-//                    } else {
-//                        $response = array(
-//                            "status" => "alert-danger",
-//                            "message" => "Robot verification failed, please try again."
-//                        );
-//                    }
-//                } else {
-//                    $response = array(
-//                        "status" => "alert-danger",
-//                        "message" => "Please check on the reCAPTCHA box."
-//                    );
-//                }
-//            } else {
-//                $response = array(
-//                    "status" => "alert-danger",
-//                    "message" => "All the fields are required."
-//                );
-//            }
-//
-//            return redirect()->to(url()->previous().'#contact')->with($response);
-//        }
+                        $header = "MIME-Version: 1.0\r\n";
+                        $header .= "Content-type: text/html\r\n";
+
+                        $body  = '<table style="padding: 35px; background-color: #f5f5f5; font-family: Roboto, sans-serif; font-size: 1rem; text-align: left; border-radius: 4px">';
+                        $body .= '<tr><th style="font-size: 1.5rem; color: #1E50BC">Full name : </th><th style="font-size: 1rem; font-weight: 500; color: #1E50BC">'.$name.'</th></tr>';
+                        $body .= '<tr><th style="font-size: 1.5rem; color: #1E50BC">Phone number : </th><th style="font-size: 1rem; font-weight: 500; color: #1E50BC">'.$phone.'</th></tr>';
+                        $body .= '<tr><th style="font-size: 1.5rem; color: #1E50BC">Email address : </th><th style="font-size: 1rem; font-weight: 500; color: #1E50BC">'.$email.'</th></tr>';
+                        $body .= '<tr><th style="font-size: 1.5rem; color: #1E50BC">Message : </th><th style="font-size: 1rem; font-weight: 500; color: #1E50BC">'.$message.'</th></tr>';
+                        $body .= '</table>';
+
+                        $mail = mail($toMail, $subject, $body, $header);
+                        if($mail){
+                            $response = array(
+                                "status" => "alert-success",
+                                "message" => "Your mail has been sent. We will get back to you shortly."
+                            );
+                        }
+                        else {
+                            $response = array(
+                                "status" => "alert-danger",
+                                "message" => "Oops, something went wrong. Please try again."
+                            );
+                        }
+
+                    } else {
+                        $response = array(
+                            "status" => "alert-danger",
+                            "message" => "Robot verification failed, please try again."
+                        );
+                    }
+                } else {
+                    $response = array(
+                        "status" => "alert-danger",
+                        "message" => "Please check on the reCAPTCHA box."
+                    );
+                }
+            } else {
+                $response = array(
+                    "status" => "alert-danger",
+                    "message" => "All the fields are required."
+                );
+            }
+
+            return redirect()->to(url()->previous().'#contact')->with($response);
+        }
 
     }
 }
