d05fbb40563dbb54e52c328ed554e9436e25b923
nellytadi <nellytadi@gmail.com>
2020-10-21 21:39:25 +0100

    Removed some partners

--- resources/views/layouts/partners.blade.php
+++ resources/views/layouts/partners.blade.php
@@ -1,11 +1,265 @@
-@extends('layouts.layout')
+<div class="row justify-content-center text-center">
+    <div class="col-12">
+        <div class="btn-group btn-group-toggle" data-toggle="buttons">
+            <label class="btn">
+                <input type="radio" value="all" checked class="btn-filter-item">
+                <span>All</span>
+            </label>
+            <label class="btn active">
+                <input type="radio" value="uk" class="btn-filter-item">
+                <span>UK</span>
+            </label>
+            <label class="btn">
+                <input type="radio" value="irish" class="btn-filter-item">
+                <span>Irish</span>
+            </label>
+            <label class="btn">
+                <input type="radio" value="us" class="btn-filter-item">
+                <span>US</span>
+            </label>
+            <label class="btn">
+                <input type="radio" value="canadian" class="btn-filter-item">
+                <span>Canadian</span>
+            </label>
+            <label class="btn">
+                <input type="radio" value="german" class="btn-filter-item">
+                <span>German</span>
+            </label>
+            <label class="btn">
+                <input type="radio" value="australian" class="btn-filter-item">
+                <span>Australian</span>
+            </label>
+            <label class="btn">
+                <input type="radio" value="netherlands" class="btn-filter-item">
+                <span>Netherlands</span>
+            </label>
+        </div>
+    </div>
+</div>
+<div class="row items filter-items">
+    <!--UK SCHOOLS-->
+    {{--<div class="col-6 col-md-4 col-lg-2 item filter-item"--}}
+    {{--data-groups='["uk"]'>--}}
+    {{--<div class="row card p-0 text-center">--}}
+    {{--<img src="{{asset('assets/images/uk-uni/uni-hertford.png')}}" alt="uni-hertford">--}}
+    {{--</div>--}}
+    {{--</div>--}}
 
-@section('styles')
+    {{--<div class="col-6 col-md-4 col-lg-2 item filter-item"--}}
+    {{--data-groups='["uk"]'>--}}
+    {{--<div class="row card p-0 text-center">--}}
+    {{--<img src="{{asset('assets/images/uk-uni/kingston.png')}}" alt="kingston">--}}
+    {{--</div>--}}
+    {{--</div>--}}
 
-@endsection
-@section('content')
+    <div class="col-6 col-md-4 col-lg-2 item filter-item"
+         data-groups='["uk"]'>
+        <div class="row card p-0 text-center">
+            <img src="{{asset('assets/images/uk-uni/birmingham.jpg')}}" alt="birmingham">
+        </div>
+    </div>
 
-@endsection
-@section('scripts')
+    <div class="col-6 col-md-4 col-lg-2 item filter-item"
+         data-groups='["uk"]'>
+        <div class="row card p-0 text-center">
+            <img src="{{asset('assets/images/uk-uni/conventry.png')}}" alt="conventry">
+        </div>
+    </div>
+    <div class="col-6 col-md-4 col-lg-2 item filter-item"
+         data-groups='["uk"]'>
+        <div class="row card p-0 text-center">
+            <img src="{{asset('assets/images/uk-uni/cransfield.png')}}" alt="cransfield">
+        </div>
+    </div>
+    <div class="col-6 col-md-4 col-lg-2 item filter-item"
+         data-groups='["uk"]'>
+        <div class="row card p-0 text-center">
+            <img src="{{asset('assets/images/uk-uni/derby.png')}}" alt="derby">
+        </div>
+    </div>
+    <div class="col-6 col-md-4 col-lg-2 item filter-item"
+         data-groups='["uk"]'>
+        <div class="row card p-0 text-center">
+            <img src="{{asset('assets/images/uk-uni/essex.png')}}" alt="essex">
+        </div>
+    </div>
+    <div class="col-6 col-md-4 col-lg-2 item filter-item"
+         data-groups='["uk"]'>
+        <div class="row card p-0 text-center">
+            <img src="{{asset('assets/images/uk-uni/kent.jpg')}}" alt="kent">
+        </div>
+    </div>
+    <div class="col-6 col-md-4 col-lg-2 item filter-item"
+         data-groups='["uk"]'>
+        <div class="row card p-0 text-center">
+            <img src="{{asset('assets/images/uk-uni/leeds.jpg')}}" alt="leeds">
+        </div>
+    </div>
+    <div class="col-6 col-md-4 col-lg-2 item filter-item"
+         data-groups='["uk"]'>
+        <div class="row card p-0 text-center">
+            <img src="{{asset('assets/images/uk-uni/liverpool.png')}}" alt="liverpool">
+        </div>
+    </div>
+    {{--<div class="col-6 col-md-4 col-lg-2 item filter-item"--}}
+    {{--data-groups='["uk"]'>--}}
+    {{--<div class="row card p-0 text-center">--}}
+    {{--<img src="{{asset('assets/images/uk-uni/nottinham.jpg')}}" alt="nottinham">--}}
+    {{--</div>--}}
+    {{--</div>--}}
+    <div class="col-6 col-md-4 col-lg-2 item filter-item"
+         data-groups='["uk"]'>
+        <div class="row card p-0 text-center">
+            <img src="{{asset('assets/images/uk-uni/nottinham-trent.jpg')}}" alt="nottinham-trentm">
+        </div>
+    </div>
+    <div class="col-6 col-md-4 col-lg-2 item filter-item"
+         data-groups='["uk"]'>
+        <div class="row card p-0 text-center">
+            <img src="{{asset('assets/images/uk-uni/surrey.jpg')}}" alt="surrey">
+        </div>
+    </div>
 
-@endsection
+    <!--END UK-->
+
+    <!--IRISH-->
+    <div class="col-6 col-md-4 col-lg-2 item filter-item"
+         data-groups='["irish"]'>
+        <div class="row card p-0 text-center">
+            <img src="{{asset('assets/images/irish-uni/griffith.jpg')}}" alt="griffith">
+        </div>
+    </div>
+
+    <div class="col-6 col-md-4 col-lg-2 item filter-item"
+         data-groups='["irish"]'>
+        <div class="row card p-0 text-center">
+            <img src="{{asset('assets/images/irish-uni/ait.png')}}" alt="ait">
+        </div>
+    </div>
+    {{--<div class="col-6 col-md-4 col-lg-2 item filter-item"--}}
+    {{--data-groups='["irish"]'>--}}
+    {{--<div class="row card p-0 text-center">--}}
+    {{--<img src="{{asset('assets/images/irish-uni/dcu.png')}}" alt="dcu">--}}
+    {{--</div>--}}
+    {{--</div>--}}
+    {{--<div class="col-6 col-md-4 col-lg-2 item filter-item"--}}
+    {{--data-groups='["irish"]'>--}}
+    {{--<div class="row card p-0 text-center">--}}
+    {{--<img src="{{asset('assets/images/irish-uni/ireland.jpg')}}" alt="ireland">--}}
+    {{--</div>--}}
+    {{--</div>--}}
+    <div class="col-6 col-md-4 col-lg-2 item filter-item"
+         data-groups='["irish"]'>
+        <div class="row card p-0 text-center">
+            <img src="{{asset('assets/images/irish-uni/limerick.png')}}" alt="limerick">
+        </div>
+    </div>
+    <!--END IRISH-->
+
+    <!--GERMAN-->
+    <div class="col-6 col-md-4 col-lg-2 item filter-item"
+         data-groups='["german"]'>
+        <div class="row card p-0 text-center">
+            <img src="{{asset('assets/images/german/gisma.png')}}" alt="gisma">
+        </div>
+    </div>
+    <!--END GERMAN-->
+
+    <!--CANADIAN-->
+    <div class="col-6 col-md-4 col-lg-2 item filter-item"
+         data-groups='["canadian"]'>
+        <div class="row card p-0 text-center">
+            <img src="{{asset('assets/images/canadian/alberta.jpg')}}" alt="alberta">
+        </div>
+    </div>
+    {{--<div class="col-6 col-md-4 col-lg-2 item filter-item"--}}
+    {{--data-groups='["canadian"]'>--}}
+    {{--<div class="row card p-0 text-center">--}}
+    {{--<img src="{{asset('assets/images/canadian/bodwell.png')}}" alt="bodwell">--}}
+    {{--</div>--}}
+    {{--</div>--}}
+    <div class="col-6 col-md-4 col-lg-2 item filter-item"
+         data-groups='["canadian"]'>
+        <div class="row card p-0 text-center">
+            <img src="{{asset('assets/images/canadian/manitoba.png')}}" alt="manitoba">
+        </div>
+    </div>
+    <div class="col-6 col-md-4 col-lg-2 item filter-item"
+         data-groups='["canadian"]'>
+        <div class="row card p-0 text-center">
+            <img src="{{asset('assets/images/canadian/taie.png')}}" alt="taie">
+        </div>
+    </div>
+    <div class="col-6 col-md-4 col-lg-2 item filter-item"
+         data-groups='["canadian"]'>
+        <div class="row card p-0 text-center">
+            <img src="{{asset('assets/images/canadian/regina.png')}}" alt="regina">
+        </div>
+    </div>
+    <div class="col-6 col-md-4 col-lg-2 item filter-item"
+         data-groups='["canadian"]'>
+        <div class="row card p-0 text-center">
+            <img src="{{asset('assets/images/canadian/windsor.png')}}" alt="windsor">
+        </div>
+    </div>
+
+    <!--END CANADIAN-->
+
+    <!--US -->
+    <div class="col-6 col-md-4 col-lg-2 item filter-item"
+         data-groups='["us"]'>
+        <div class="row card p-0 text-center">
+            <img src="{{asset('assets/images/us/utah.png')}}" alt="utah">
+        </div>
+    </div>
+    <div class="col-6 col-md-4 col-lg-2 item filter-item"
+         data-groups='["us"]'>
+        <div class="row card p-0 text-center">
+            <img src="{{asset('assets/images/us/pace.jpg')}}" alt="pace">
+        </div>
+    </div>
+    <div class="col-6 col-md-4 col-lg-2 item filter-item"
+         data-groups='["us"]'>
+        <div class="row card p-0 text-center">
+            <img src="{{asset('assets/images/us/northern.jpg')}}" alt="northern">
+        </div>
+    </div>
+    <div class="col-6 col-md-4 col-lg-2 item filter-item"
+         data-groups='["us"]'>
+        <div class="row card p-0 text-center">
+            <img src="{{asset('assets/images/us/american.png')}}" alt="american">
+        </div>
+    </div>
+    <!--END US-->
+
+    <!--AUSTRALIA-->
+    <div class="col-6 col-md-4 col-lg-2 item filter-item"
+         data-groups='["australian"]'>
+        <div class="row card p-0 text-center">
+            <img src="{{asset('assets/images/australia/melbourne.png')}}" alt="melbourne">
+        </div>
+    </div>
+    <div class="col-6 col-md-4 col-lg-2 item filter-item"
+         data-groups='["australian"]'>
+        <div class="row card p-0 text-center">
+            <img src="{{asset('assets/images/australia/sydney.png')}}" alt="sydney">
+        </div>
+    </div>
+    <!--END AUSTRALIA-->
+
+    <!--NETHERLANDS -->
+    <div class="col-6 col-md-4 col-lg-2 item filter-item"
+         data-groups='["netherlands"]'>
+        <div class="row card p-0 text-center">
+            <img src="{{asset('assets/images/netherlands/hague.png')}}" alt="hague">
+        </div>
+    </div>
+    <div class="col-6 col-md-4 col-lg-2 item filter-item"
+         data-groups='["netherlands"]'>
+        <div class="row card p-0 text-center">
+            <img src="{{asset('assets/images/netherlands/twente.png')}}" alt="twente">
+        </div>
+    </div>
+    <!--END NETHERLANDS -->
+    <div class="col-1 filter-sizer"></div>
+</div>
